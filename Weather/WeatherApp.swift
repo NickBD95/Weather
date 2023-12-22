d24f4e647ed2b7b2916315b1e084ff3874b95330
Nickolai Grachev <nickbd95@gmail.com>
2023-12-22 19:23:20 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Nikolai Grachev on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
